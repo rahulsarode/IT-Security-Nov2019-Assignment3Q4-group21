657f5485b339871917ea7d2ab003a5ae8e8b80ab
Rahul Sarode <rahulsarode9@gmail.com>
2019-11-07 16:13:27 +0100

    fix for CSRF vulnerability @ login service

--- forum/src/main/java/com/forum/controller/UserController.java
+++ forum/src/main/java/com/forum/controller/UserController.java
@@ -50,17 +50,17 @@ public class UserController {
 	}
 	
 	
-	//*****************UNSAFE*****************
+	/*//*****************UNSAFE*****************
 	@CrossOrigin(origins = "*")
 	@PostMapping("/login")
 	public ResponseEntity<UserDetailsModel> validateLogin(@RequestBody UserDetailsModel userModel) throws SQLException {
 		return userService.validateUser(userModel);
-	}
-	/*//*****************SAFE*****************
+	}*/
+	//*****************SAFE*****************
 	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/login")
 	public ResponseEntity<UserDetailsModel> validateLogin(@RequestBody UserDetailsModel userModel) throws SQLException {
 		return userService.validateUser(userModel);
-	}*/
+	}
 }
 
